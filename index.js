6321cc6c104c1785a33e0b067cceb410746ba97b
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-05-11 22:02:09 -0400

    Added reactive clock, started function to check and compare the current hour with the hour block

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-// Display date/time on page
 
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+
+
+
+
+// Add currenmt date and time and keep time without refeshing the browser
+
+var updateTime = function() {
+    document.getElementById("currentTime")
+    .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+setInterval(updateTime, 1000);
+
+// Create function to 
+
+
+
+
+// Create a function to add the different colors
+
+function checkHour() {
+    const currentHour = moment().hours();
+    // Read the hourBlock hour in the HTML 
+    $(".time-block").each(function() {
+        const hourBlock = parsInt(
+            $(this) 
+            .attr("id")
+            .split("-")[1],
+            
+        );
+       
+
+    });
+    // if (hour > hourBlock ) {
+    //     .addClass("past";)
+    // } else if (hour === hourBlock) {
+    //     .removeClass("past");
+    //     .removeClass("future");
+    //     .addClass("present");
+    // } else if (hour < hourBlock){
+    //     .removeClass("past");
+    //     .removeClass("present");
+    //     .addClass("future");
+    // }
+};
+
+
+checkHour();
+
+
